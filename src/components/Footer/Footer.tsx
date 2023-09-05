9e14c1ea54e58dac8cdb5cb86d2a2b9b1960ae8b
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-09-05 18:03:02 -0300

    Creating footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <small> © 2023 / Luis Braga Dev - Todos os direitos reservados </small>
+    </footer>
+  )
+}
+
+export default Footer
