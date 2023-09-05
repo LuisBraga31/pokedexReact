cd1fc4de9e7a4298ef9f7d10e32fa3582e2222c5
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-09-05 17:20:41 -0300

    Creating header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+
+    <header className={styles.header}>
+        <img src="../../../public/assets/logo.png" alt="pokedex" />
+    </header>
+
+  )
+}
+
+export default Header
