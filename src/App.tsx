51dc3b795cd9a0ee7813894789949b25ed7edda1
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-09-05 17:40:00 -0300

    Creating pokedex body

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Pokedex from './components/Pokedex/Pokedex'
 
 function App() {
   
   return (
     <>
       <Header/>
+      <Pokedex/>
     </>
   )
 }
---
+++ src/components/Pokedex/Pokedex.tsx
@@ -0,0 +1,25 @@
+import styles from './Pokedex.module.css';
+
+const Pokedex = () => {
+  return (
+    <main className={styles.main}>
+
+        <img src="#" alt="pokemon" className={styles.pokemonImage}/>
+
+        <div className={styles.pokemonData}>
+            <span className={styles.pokemonNumber}> 01 </span> -
+            <span className={styles.pokemonName}> Bulbassaur </span>
+        </div>
+
+        <div className={styles.pokeButtons}>
+            <button> Ant </button>
+            <button> Next </button>
+        </div>
+
+        <img src="../../../public/assets/pokedex.png" alt="" className={styles.pokedexBack}/>
+
+    </main>
+  )
+}
+
+export default Pokedex
